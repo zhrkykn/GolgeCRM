852d6a01f64aa86637d70bca140e2d6d63c7b375
Halid <hmyazicioglu_7@hotmail.com>
2022-03-30 16:37:02 +0300

    I created step defs and filled feature file

--- src/test/java/com/azulcrm/step_defs/PollFunctionStepDefs.java
+++ src/test/java/com/azulcrm/step_defs/PollFunctionStepDefs.java
@@ -1,7 +1,67 @@
 package com.azulcrm.step_defs;
 
+import com.azulcrm.page.PollFunctionPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class PollFunctionStepDefs {
 
+    PollFunctionPage pfp = new PollFunctionPage();
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user lands on Activity Stream page and clicks on Poll")
+    public void the_user_lands_on_Activity_Stream_page_and_clicks_on_Poll() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user selects multiple contacts from Employees and Departments")
+    public void the_user_selects_multiple_contacts_from_Employees_and_Departments() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user attach a link by clicking on the link icon")
+    public void the_user_attach_a_link_by_clicking_on_the_link_icon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user add mention by clicking on the mention icon")
+    public void the_user_add_mention_by_clicking_on_the_mention_icon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user adds questions and multiple answers")
+    public void the_user_adds_questions_and_multiple_answers() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user delete questions and multiple answers")
+    public void the_user_delete_questions_and_multiple_answers() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user provides multiple choice to attendees by selecting Allow multiple choice checkbox")
+    public void the_user_provides_multiple_choice_to_attendees_by_selecting_Allow_multiple_choice_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user clicks send button and sends the poll")
+    public void the_user_clicks_send_button_and_sends_the_poll() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 
 }
--- src/test/resources/features/poll.feature
+++ src/test/resources/features/poll.feature
@@ -2,7 +2,23 @@
   Feature: Creating Poll Function
 
 
-    Scenario: As a user, I should be able to create a poll by clicking on Poll tab under Active Stream
+    Scenario Outline: Create a Poll
+      Given the user logged in as "<userType>"
+      When the user lands on Activity Stream page and clicks on Poll
+      Then the user selects multiple contacts from Employees and Departments
+      And the user attach a link by clicking on the link icon
+      Then the user add mention by clicking on the mention icon
+      And the user adds questions and multiple answers
+      Then the user delete questions and multiple answers
+      And the user provides multiple choice to attendees by selecting Allow multiple choice checkbox
+      Then the user clicks send button and sends the poll
+
+
+      Examples:
+      |   userType                          |
+      |   helpdesk1@cybertekschool.com      |
+      |   hr1@cybertekschool.com            |
+      |   marketing2@cybertekschool.com     |
 
 
 
